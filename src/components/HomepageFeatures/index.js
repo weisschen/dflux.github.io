db88b14612ba6e98034ae55da0f065c8df9155c4
Chen Weissman <chenw@Chens-MacBook-Air.local>
2022-08-15 00:52:16 +0300

    initial commit - docusaurus lp

---
+++ src/components/HomepageFeatures/index.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+
+const FeatureList = [
+  {
+    title: 'Distributed Architecture',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        allows the platform to scale horizontally up to millions of emulated subscribers and devices 
+        at rates of tens of thousands of events per second and traffic of Gigabits per second.
+      </>
+    ),
+  },
+  {
+    title: 'Developer Friendly',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+        provides the best in class yet easy to use functional, performance and security test options to service providers, interoperable system’s vendors and individual contributors.
+      </>
+    ),
+  },
+  {
+    title: 'CI/CT/CD Driven',
+    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+         Vast integrations to CI tools without the need for protocol expertise creates robust capabillities 
+         for customizing every detail in tests of complex flows and scenarios and their correspondent metrics, automatically.
+      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Basic from '@site/src/components/ContactForm';
+
+
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        {/* <div className={styles.buttons}>
+        <Link
+            className={clsx(
+              'button button--outline button--secondary button--lg',
+              styles.heroButton
+            )}
+            to="/docs/intro">
+            Book a Demo
+          </Link>
+        </div> */}
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+      <div className={styles.forms}>
+        <p>  <Basic /></p>
+        </div>
+    </Layout>
+  );
+}
