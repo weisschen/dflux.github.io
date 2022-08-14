6aaf51628e98586809dfcdfd9feb9e270c17c5ed
Chen Weissman <chenw@Chens-MacBook-Air.local>
2022-08-15 02:24:42 +0300

    configured meta image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -56,6 +56,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: 'img/dflux.png',
       navbar: {
         title: 'Dflux',
         logo: {
