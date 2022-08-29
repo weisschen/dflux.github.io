992b46d156538159026667a9b240d6940873758b
Chen Weissman <chenw@Chens-MacBook-Air.local>
2022-08-29 23:03:57 +0300

    format

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -3,35 +3,35 @@ import { Formik } from 'formik';
 import styles from './styles.module.css';
 
 
- var Airtable = require('airtable');
- var base = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(process.env.AIRTABLE_BASE_ID);
- 
- const Basic = () => (
-   <div className="contactForm">
-     <h2>Let's Talk</h2>
-     <Formik
-       initialValues={{ email: ''}}
-       validate={values => {
-         const errors = {};
-         if (!values.email) {
-           errors.email = 'Required';
-         } else if (
-           !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-         ) {
-           errors.email = 'Invalid email address';
-         }
-         return errors;
-       }}
-       onSubmit={(values, { setSubmitting, setStatus, resetForm }) => {
-         setTimeout(() => {
-           base('mail_list').create([
+var Airtable = require('airtable');
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(process.env.AIRTABLE_BASE_ID);
+
+const Basic = () => (
+  <div className="contactForm">
+    <h2>Let's Talk</h2>
+    <Formik
+      initialValues={{ email: '' }}
+      validate={values => {
+        const errors = {};
+        if (!values.email) {
+          errors.email = 'Required';
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = 'Invalid email address';
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting, setStatus, resetForm }) => {
+        setTimeout(() => {
+          base('mail_list').create([
             {
               "fields": {
                 "mail": values.email,
                 "timestamp": Date.now()
               }
             }
-          ], function(err, records) {
+          ], function (err, records) {
             if (err) {
               console.error(err);
               resetForm()
@@ -50,64 +50,63 @@ import styles from './styles.module.css';
               });
             });
           });
-           setSubmitting(false);
-           setTimeout(
+          setSubmitting(false);
+          setTimeout(
             () => setStatus({
               sent: '',
               msg: ""
-            }), 
+            }),
             3000
           );
-         }, 400);
-       }}
-     >
-       {({
-         values,
-         errors,
-         touched,
-         handleChange,
-         handleBlur,
-         handleSubmit,
-         isSubmitting,
-         status
-         /* and other goodies */
-       }) => (
-         <form onSubmit={handleSubmit}>
-            <div>
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        status
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
             <br></br>
-           <input
-            class={styles.mailInput}
-             type="email"
-             name="email"
-             placeholder="Enter your email"
-             onChange={handleChange}
-             onBlur={handleBlur}
-             value={values.email}
-           />
-                      <br></br>
-           {errors.email && touched.email && (
-            <div className={styles.errorMail}>{errors.email}</div>
-          )}
-           </div>
-           <br></br>
-           <div>
-           {status && status.msg && (
+            <input
+              class={styles.mailInput}
+              type="email"
+              name="email"
+              placeholder="Enter your email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+            <br></br>
+            {errors.email && touched.email && (
+              <div className={styles.errorMail}>{errors.email}</div>
+            )}
+          </div>
+          <br></br>
+          <div>
+            {status && status.msg && (
               <p
-                className={`{styles.alert} ${
-                  status.sent ? styles.alertSuccess : styles.alertError
-                }`}
+                className={`{styles.alert} ${status.sent ? styles.alertSuccess : styles.alertError
+                  }`}
               >
                 {status.msg}
               </p>
             )}
-           <button class={styles.buttons} type="submit" disabled={isSubmitting}>
-             Submit {isSubmitting && <span>Sending...</span>}
-           </button>
-           </div>
-         </form>
-       )}
-     </Formik>
-   </div>
- );
- 
- export default Basic;
+            <button class={styles.buttons} type="submit" disabled={isSubmitting}>
+              Submit {isSubmitting && <span>Sending...</span>}
+            </button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default Basic;
